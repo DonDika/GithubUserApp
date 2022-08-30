f9cf2a187f2bfa599faf7fad38373335678390c4
IkhsanMardika <ikhsancahyamardika@gmail.com>
2022-08-30 14:36:54 +0800

    Delete local data & set token API

--- app/src/main/java/com/dondika/githubuserapp/ui/HomeActivity.kt
+++ app/src/main/java/com/dondika/githubuserapp/ui/HomeActivity.kt
@@ -4,17 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.dondika.githubuserapp.data.GithubUserData
 import com.dondika.githubuserapp.data.User
-import com.dondika.githubuserapp.databinding.ActivityMainBinding
+import com.dondika.githubuserapp.databinding.ActivityHomeBinding
 
-class MainActivity : AppCompatActivity() {
+class HomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityHomeBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityHomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setAdapter()
@@ -22,16 +21,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun setAdapter() {
         val userAdapter = UserAdapter()
-        userAdapter.setListUsers(GithubUserData.listUser(this@MainActivity))
+       // userAdapter.setListUsers(GithubUserData.listUser(this@HomeActivity))
 
         binding.rvUsers.apply {
             adapter = userAdapter
-            layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.VERTICAL, false)
+            layoutManager = LinearLayoutManager(this@HomeActivity, LinearLayoutManager.VERTICAL, false)
         }
 
         userAdapter.setOnItemClickCallback(object : UserAdapter.OnItemClickCallback{
             override fun onItemClicked(user: User) {
-                val intent = Intent(this@MainActivity, DetailActivity::class.java)
+                val intent = Intent(this@HomeActivity, DetailActivity::class.java)
                 intent.putExtra(DetailActivity.EXTRA_USER, user)
                 startActivity(intent)
             }
