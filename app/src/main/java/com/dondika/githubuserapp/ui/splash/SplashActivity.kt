cf558e50aeb6ca250b3776b16f6999a29df449b1
IkhsanMardika <ikhsancahyamardika@gmail.com>
2022-09-02 10:20:53 +0800

    Add splash activtiy

---
+++ app/src/main/java/com/dondika/githubuserapp/ui/splash/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.dondika.githubuserapp.ui.splash
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.dondika.githubuserapp.databinding.ActivitySplashBinding
+import com.dondika.githubuserapp.ui.home.HomeActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        supportActionBar?.hide()
+
+        setSplash()
+    }
+
+    private fun setSplash() {
+        val handler = Handler(mainLooper)
+        handler.postDelayed({
+            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            finish()
+        },2000L)
+
+    }
+
+}
